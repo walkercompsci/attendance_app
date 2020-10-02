66eb8969be0be2db35436c4dd5fa932a9bedec4e
mcintril000 <mcintril000@warren.k12.in.us>
2020-10-02 10:52:56 -0400

    Made login thing route to the get date page

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'login.dart';
+import 'package:attendance_app/pages/login.dart';
 
 void main() => runApp(MaterialApp(
   home: Login(),
---
+++ lib/pages/login.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+
+class Login extends StatefulWidget {
+  @override
+  _LoginState createState() => _LoginState();
+}
+
+class _LoginState extends State<Login> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        backgroundColor: Colors.grey,
+        appBar: AppBar(
+          title: Text('Attendance Club App'),
+          centerTitle: true,
+          backgroundColor: Colors.black,
+        ),
+        body: SafeArea(
+          child: Column(
+            children: <Widget>[
+              FlatButton.icon(
+                onPressed:(){
+                  Navigator.pushNamed(context,'/main');
+                },
+                icon: Icon(Icons.edit_location),
+                label: Text('edit location'),
+              ),
+            ],
+        ),
+        ),
+      );
+    }
+  }
+

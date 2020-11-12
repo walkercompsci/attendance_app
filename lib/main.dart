2367975a29bc04a081186c45955fc7514293653c
taylokol000 <taylokol000@warren.k12.in.us>
2020-11-12 10:30:32 -0500

    added firebase so it can be used in the future

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:attendance_app/pages/add.dart';
 import 'package:attendance_app/pages/teacherpage.dart';
 import 'package:flutter/material.dart';
 import 'package:attendance_app/pages/login.dart';
+import 'package:firebase_core/firebase_core.dart';
 
 void main() => runApp(MaterialApp(
   home: LoginButton(),
@@ -119,3 +120,31 @@ class _GetDateState extends State<GetDate> {
         );
   }
 }
+
+/*class App extends StatelessWidget {
+  // Create the initialization Future outside of `build`:
+  final Future<FirebaseApp> _initialization = Firebase.initializeApp();
+
+  @override
+  Widget build(BuildContext context) {
+    return FutureBuilder(
+      // Initialize FlutterFire:
+      future: _initialization,
+      builder: (context, snapshot) {
+        // Check for errors
+        if (snapshot.hasError) {
+          return SomethingWentWrong();
+        }
+
+        // Once complete, show your application
+        if (snapshot.connectionState == ConnectionState.done) {
+          return MyAwesomeApp();
+        }
+
+        // Otherwise, show something whilst waiting for initialization to complete
+        return Loading();
+      },
+    );
+  }
+}*/
+
